e5688ebd539c1e623235bb55b15955007e1d9368
Veniu <65432279+GrupoVeniu@users.noreply.github.com>
2022-11-10 09:31:47 -0600

    Update Purchase Corrections

--- purchase_extra_fields/models/purchase_extra_fields.py
+++ purchase_extra_fields/models/purchase_extra_fields.py
@@ -2,6 +2,7 @@ from odoo import fields, models
 
 class purchaseExtra(models.Model):
     _inherit = 'purchase.order'
-
-    motivo_de_uso = fields.Selection ([('C', 'Consumo), ('O', 'Otros), ('V', 'Venta')], string="Motivos de uso")
-
+    
+    motivo_de_uso = fields.Selection([('c','Consumo'), ('v','Venta'), ('r','Reposicion')], string="Motivo de Uso", required=True)
+    # desarrollo = fields.Selection([('I','Iarará'), ('OS','OmnivitaliaSur'), ('OC','Omnivitalia Chapalita')], string="Desarrollo de interés", required=True)
+    # interes = fields.Selection([('D','Renta'), ('I','Venta'), ('OS','Inversión')], string="Interés", required=True)
