d883622ad98400ff7daee9179898b80e3e8cc59b
Veniu <65432279+GrupoVeniu@users.noreply.github.com>
2022-11-14 10:55:46 -0600

    Update controllers.py

--- el_modulo/controllers/controllers.py
+++ el_modulo/controllers/controllers.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import Response
+import json
 
 
-# class MyModule(http.Controller):
-#     @http.route('/my_module/my_module', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class MyModule(http.Controller):
+    @http.route('/api/attendance/hours/<user_id>/<day_week>', auth='public', methods=['GET'])
+    def get_attendances_hours(self, user_id, day_week, **kw):
+        try:
+            calendar_id = http.request.env['hr.employee'].sudo()\
+                .search([('user_id', '=', int(user_id))]).resource_calendar_id
+            hours = http.request.env['resource.calendar.attendance'].sudo()\
+                .search_read([('calendar_id', '=', int(calendar_id)), ('dayofweek', '=', day_week)], 
+                ['hour_from', 'hour_to'])
+        except Exception as e:
+            return build_response ({'err': str(e)})
+
+    def build_response(self, entity):
+        response = json.dumps(entity, ensure_ascii=False).encode('utf8')
+        return Response (response, content_type='aplication/json;charset=utf-8',status=200)
 
 #     @http.route('/my_module/my_module/objects', auth='public')
 #     def list(self, **kw):
